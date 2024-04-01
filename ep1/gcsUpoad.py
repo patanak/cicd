1f4f52de1cc732eb6b4d005d0b78af2cd4f7f4fa
patanak <patanak2526@gmail.com>
2024-04-01 16:22:05 +0700

    add bucket

--- ep1/gcsUpoad.py
+++ ep1/gcsUpoad.py
@@ -1,5 +1,5 @@
 from google.cloud import storage
 storage_client = storage.Client()
-bucket = storage_client.get_bucket("btg_aecorsoft_bucket")
+bucket = storage_client.get_bucket("btg_cicd_2")
 blob = bucket.blob('cicd_2/newtxt.txt')
 blob.upload_from_filename('./test.txt')
